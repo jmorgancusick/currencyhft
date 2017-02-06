0a2908d850e3da478273627ed4ad5a67ecdd5b10
SDSddD <losthuntet27@gmail.com>
2017-02-06 17:23:22 -0500

    Added basic node template. Could add node_modules to .gitignore, but it's okay for now

---
+++ node/app.js
@@ -0,0 +1,21 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var logger = require('morgan');
+var exec = require('child_process').exec;
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));  //all html in public
+
+console.log("__dirname: "+__dirname);
+
+app.listen(3000, function () {
+  console.log('photosbyjac.com node server listening on port 3000');
+});
