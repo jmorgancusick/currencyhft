7903a159dce95f71363835575b16596861b1d64e
Patrick Engelsman <pengelsman7@gmail.com>
2017-03-20 12:10:05 -0400

    added spaces so the schema would work

--- database/schema.sql
+++ database/schema.sql
@@ -5,52 +5,52 @@ drop table if exists efficient_trends;
 
 
 create table forex ( -- extraneous probly
-	datetime		varchar(12)	-- hhmmssddMMyy
-	, exstr			varchar(40)	-- ie "USDEUR" for usd to eur
-	, start			varchar(40) 	-- might not need this abv
-	, start_full		varchar(255) 	-- full name of currency
-	, end			varchar(40) 
-	, end_full		varchar(255)
-	, rate			float		-- value of second given 1 of first
-	, ex_id			varchar(40)	-- hash based on start and end currency
-	, primary key(datetime, exstr)
+	datetime 		varchar(40)	-- hhmmssddMMyy
+	, exstr	 		varchar(40)	-- ie "USDEUR" for usd to eur
+	, start	 		varchar(40) 	-- might not need this abv
+	, start_full 		varchar(255) 	-- full name of currency
+	, end 			varchar(40) 
+	, end_full 		varchar(255)
+	, rate 			float		-- value of second given 1 of first
+	, ex_id 		varchar(40)	-- hash based on start and end currency
+	, primary key(datetime, exstr) 
 );
 
 create table bank_rates (	-- extraneous probly
-	start			varchar(40) 	-- starting currency abv
-	, start_full		varchar(255) 	-- full name of currency
-	, end			varchar(40) 
-	, end_full		varchar(255)
-	, bank 			varchar(40)	-- bank name abv
-	, bank_full		varchar(255)
-	, rate			float		-- value of second given 1 of first
-	, ex_id			varchar(40)	primary key	-- hash based on start, end currency, and bank
+	start 			varchar(40)  	-- starting currency abv
+	, start_full 		varchar(255)  	-- full name of currency
+	, end 			varchar(40) 
+	, end_full 		varchar(255)
+	, bank 	 		varchar(40)	-- bank name abv
+	, bank_full 		varchar(255)
+	, rate	 		float		-- value of second given 1 of first
+	, ex_id	 		varchar(40)	primary key	-- hash based on start, end currency, and bank
 );
 
 
 create table profitable_trends(
-	start			varchar(40) 	-- starting currency abv
-	, start_full		varchar(255) 	-- full name of currency
-	, end			varchar(40) 
-	, end_full		varchar(255)
-	, profit 		float 		-- profit based on initial currency
-	, length		int 		-- length of path
-	, expath  		varchar(500)	-- in form "USDCITI|EURCITI|USDWELLS"
+	start	 		varchar(40) 	-- starting currency abv
+	, start_full 		varchar(255) 	-- full name of currency
+	, end	 		varchar(40) 
+	, end_full 		varchar(255)
+	, profit  		float 		-- profit based on initial currency
+	, length 		int 		-- length of path
+	, expath   		varchar(500)	-- in form "USDCITI|EURCITI|USDWELLS"
 						-- to go from USD in Citi bank to Euros in Citi to USD in wells fargo
-	, frequency		int
-	, path_id		varchar(40)	primary key	-- hashed based on expath variable
+	, frequency 		int
+	, path_id 		varchar(40)	primary key	-- hashed based on expath variable
 );
 
 
 create table efficient_trends(
-	start			varchar(40) 	-- starting currency abv
-	, start_full		varchar(255) 	-- full name of currency
-	, end			varchar(40) 
-	, end_full		varchar(255)
-	, end_value		float 		-- value based on initial currency
-	, length		int 		-- length of path
-	, expath  		varchar(500)	-- in form "USDCITI|EURCITI|USDWELLS"
+	start	 		varchar(40) 	-- starting currency abv
+	, start_full 		varchar(255) 	-- full name of currency
+	, end	 		varchar(40) 
+	, end_full 		varchar(255)
+	, end_value 		float 		-- value based on initial currency
+	, length 		int 		-- length of path
+	, expath   		varchar(500)	-- in form "USDCITI|EURCITI|USDWELLS"
 						-- to go from USD in Citi bank to Euros in Citi to USD in wells fargo
-	, frequency		int
-	, path_id		varchar(40)	primary key	-- hashed based on expath variable
+	, frequency 		int
+	, path_id 		varchar(40)	primary key	-- hashed based on expath variable
 );
