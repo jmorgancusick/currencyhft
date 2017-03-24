e78003d467d24d38d3a3de3f5f01cf1e655bd9d1
Yush Yush <yush.mishra@gmail.com>
2017-03-23 20:55:26 -0400

    adds routes.js

--- node/public/routes.js
+++ node/public/routes.js
@@ -23,4 +23,50 @@ const CurrencyRouter = () => (
 	</Router>
 )
 
-export CurrencyRouter
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const Dashboard = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Arbitrage = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Arbitrage = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+export default CurrencyRouter
