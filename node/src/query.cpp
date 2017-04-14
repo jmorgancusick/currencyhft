ad0985aa7773a4a8b5100d505d47954345edaa72
Matthew Cordone <matthew.cordone@gmail.com>
2017-04-14 17:17:46 -0400

    query arb

--- node/src/query.cpp
+++ node/src/query.cpp
@@ -288,6 +288,126 @@ void ChartData(const FunctionCallbackInfo<Value>& args) {
 }
 
 
+
+void ArbitrageData(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  cout<<"Number of args: "<<args.Length()<<endl;
+
+  //Make sure 6 arguments.  Cast to string
+  if (args.Length() != 6) {
+    isolate->ThrowException(v8::String::NewFromUtf8(isolate, "Wrong number of arguments"));
+    return;
+  }
+
+  if (!args[0]->IsString()) {
+    isolate->ThrowException(v8::String::NewFromUtf8(isolate, "Wrong arguments"));
+    return;
+  }
+
+  v8::String::Utf8Value param1(args[0]->ToString());
+  std::string startCurr = std::string(*param1);
+  
+  if (!args[1]->IsString()) {
+    isolate->ThrowException(v8::String::NewFromUtf8(isolate, "Wrong arguments"));
+    return;
+  }
+
+  v8::String::Utf8Value param2(args[1]->ToString());
+  std::string endCurr = std::string(*param2);
+
+  if (!args[2]->IsDouble()) {
+    isolate->ThrowException(v8::String::NewFromUtf8(isolate, "Wrong arguments"));
+    return;
+  }
+
+  v8::String::Utf8Value param3(args[2]->ToString());
+  std::string startValue = std::string(*param3);
+
+  if (!args[3]->IsDouble()) {
+    isolate->ThrowException(v8::String::NewFromUtf8(isolate, "Wrong arguments"));
+    return;
+  }
+
+  v8::String::Utf8Value param4(args[3]->ToString());
+  std::string endValue = std::string(*param4);
+
+  if (!args[4]->IsArray()) {
+    isolate->ThrowException(v8::String::NewFromUtf8(isolate, "Wrong arguments"));
+    return;
+  }
+
+  std::vector<std:string> currenciesToExclude = new std::vector<std::string>();
+  for(int i=0; i<args[4].length(); i++) {
+    currenciesToExclude.push_back(args[4][i]->ToString());
+  }
+
+  if (!args[5]->IsInteger()) {
+    isolate->ThrowException(v8::String::NewFromUtf8(isolate, "Wrong arguments"));
+    return;
+  }
+
+  v8::String::Utf8Value param6(args[5]->ToString());
+  std::string maxNumberExchanges = std::string(*param6);
+  
+  //TODO: move this to init
+  API *db = new API();
+  int retVal = db->connect();
+  if(retVal != 0){
+    cout<<"db.connect() failed with exit code "<<retVal<<endl;
+    isolate->ThrowException(v8::String::NewFromUtf8(isolate, "Failed to connect to database"));
+    return;
+  }
+
+  string interval = "day";
+  long startDate = 1450000000;
+  long endDate = 1490064000;
+  vector<API::chart_info> *rows = db->selectHistoricalTickerData(ticker, interval,startDate,endDate);    //Database query
+  //unordered_map<std::string, double> *rows = db->selectHistoricalTickerData(ticker, interval);   //Database query
+//TODO change data structure to array of struct
+
+
+  int i = 0;
+
+  Local<Array> result = Array::New(isolate);
+  
+
+  for(vector<API::chart_info>::iterator itr = rows->begin(); itr != rows->end(); itr++, i++){
+    cout << itr->timestamp << "\t" << itr->ticker << "\t" << itr->high << "\t" << itr->volume << "\t"
+          << itr->open << "\t" << itr->low << "\t" << itr->close << endl;
+
+    // Creates a new Object on the V8 heap
+    Local<Object> obj = Object::New(isolate);
+
+    //Can call a pack function here to be cleaner once more data
+    // Transfers the data from result, to obj (see below)
+    obj->Set(String::NewFromUtf8(isolate, "date"),
+       Number::New(isolate, itr->timestamp));
+    obj->Set(String::NewFromUtf8(isolate, "ticker"),
+       String::NewFromUtf8(isolate, itr->ticker.data()));
+    obj->Set(String::NewFromUtf8(isolate, "high"),
+       Number::New(isolate, itr->high));
+    obj->Set(String::NewFromUtf8(isolate, "volume"),
+       Number::New(isolate, itr->volume));
+    obj->Set(String::NewFromUtf8(isolate, "open"),
+       Number::New(isolate, itr->open));
+    obj->Set(String::NewFromUtf8(isolate, "low"),
+       Number::New(isolate, itr->low));
+    obj->Set(String::NewFromUtf8(isolate, "close"),
+       Number::New(isolate, itr->close));
+    
+
+    result->Set(i, obj); 
+  }
+
+  args.GetReturnValue().Set(result);
+  //args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+  
+  delete db;
+}
+
+
+
 void init(Local<Object> exports) {
   NODE_SET_METHOD(exports, "exchange", Exchange);
   NODE_SET_METHOD(exports, "table", Table);
