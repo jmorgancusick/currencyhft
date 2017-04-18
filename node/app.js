4661a737866f6f40ecefdb95713541d3d6b4408e
Yush Yush <yush.mishra@gmail.com>
2017-04-18 00:12:19 -0400

    app.js function additions

--- node/app.js
+++ node/app.js
@@ -69,3 +69,21 @@ app.get('/chartData/:ticker/:timeframe', function(req, res) {
 
     res.send(ret);
 });
+
+app.get('/arbitrageData/', function(req, res) {
+    console.log(req.params.);
+
+    ret = addon.arbitrageData();
+    console.log(ret);
+
+    res.send(ret);
+});
+
+app.get('/calculatorData/', function(req, res) {
+    console.log(req.params.);
+
+    ret = addon.calculatorData();
+    console.log(ret);
+
+    res.send(ret);
+});
