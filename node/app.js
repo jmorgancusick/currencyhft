04a7269f52cd64e2f5df6c05a17b947a329e7823
John Cusick <losthuntet27@gmail.com>
2017-04-20 21:17:00 -0400

    Cleaning up tests and public. Comments added to app.js

--- node/app.js
+++ node/app.js
@@ -1,9 +1,16 @@
-/**
- * Module dependencies.
- */
+/* app.js
+Data server used for serving Forex and bank data to the frontend via a REST_API.
+Algorithms are also exposed to the frontend.
+*/
 
+// ===================
+// Server Dependencies
+// ===================
+
+// C++ Binary
 const addon = require('./src/build/Release/addon');
 
+// JavaScript Libraries
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
@@ -12,99 +19,25 @@ var exec = require('child_process').exec;
 var bodyParser = require('body-parser');
 var app = express();
 
+// =====================
+// Server Initialization
+// =====================
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));  //all html in public
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-
-console.log("__dirname: "+__dirname);
-
 app.listen(3000, function () {
   console.log('photosbyjac.com node server listening on port 3000');
 });
 
-app.get('/exchange/:id', function(req, res) {
-
-    // log id
-    console.log(req.params.id);
-
-    ret = addon.exchange(req.params.id);
-    console.log(ret);
-    
-    res.send(ret);
-});
-
-app.get('/table/:tableName', function(req, res) {
-    // log table name
-    console.log(req.params.tableName);
-
-    ret = addon.table(req.params.tableName);
-    console.log(ret);
-
-    res.send(ret);
-});
-
-app.get('/tickerData/', function(req, res) {
-    // function takes 0 args (automatic load)
-
-    ret = addon.tickerData();
-    console.log(ret);
-    
-    res.send(ret);
-
-});
-
-app.get('/chartData/:ticker/:timeframe/:startDate/:endDate', function(req, res) {
-    // log chartId
-    console.log(req.params.ticker);
-    console.log(req.params.timeframe);
-    console.log(req.params.startDate);
-    console.log(req.params.endDate);
-
-    ret = addon.chartData(req.params.ticker,req.params.timeframe,req.params.startDate,req.params.endDate);
-    console.log(ret);
-
-    res.send(ret);
-});
-
-app.get('/arbitrageData/:startCurr/:endCurr/:maxNumberExchanges', function(req, res) {
-    console.log("arbitrage data endpoint");
-
-    console.log("startCurr: ",req.params.startCurr);
-    console.log("endCurr: ",req.params.endCurr);
-    console.log("exclude: ",req.query.exclude);
-    console.log("maxNumExch: ",req.params.maxNumberExchanges);
-
-    if (req.query.exclude === undefined){
-        req.query.exclude = [];
-    }
-
-    ret = addon.arbitrageData(req.params.startCurr, req.params.endCurr, req.params.maxNumberExchanges, req.query.exclude);
-    console.log(ret);
-
-    res.send(ret);
-});
-
-app.get('/calculatorData/:startCurr/:endCurr', function(req, res) {
-    console.log("calculator data endpoint");
-
-    ret = addon.calculatorData(req.params.startCurr, req.params.endCurr);
-    console.log(ret);
-
-    if(isNaN(ret["rate"])){
-        console.log("rate is not a number, returning 1.")
-        ret["rate"] = 1;
-    }
-
-    res.send(ret);
-});
-
-
+// =============
+// Exit handling
+// =============
 
 function exitHandler(options, err) {
     if (options.cleanup){
@@ -116,7 +49,7 @@ function exitHandler(options, err) {
     if (options.exit) process.exit();
 }
 
-//do something when app is closing
+//cleanup when app is closing
 process.on('exit', exitHandler.bind(null,{cleanup:true}));
 
 //catches ctrl+c event
@@ -124,3 +57,56 @@ process.on('SIGINT', exitHandler.bind(null, {exit:true}));
 
 //catches uncaught exceptions
 process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
+// ==============
+// Data Endpoints
+// ==============
+
+// Return the 18 most popular currency pairs along with their most recent exchange rate
+app.get('/tickerData/', function(req, res) {
+    console.log("ticker data endpoint");
+
+    ret = addon.tickerData();
+    
+    res.send(ret);
+
+});
+
+// Return high, low and close exchange rates for single ticker over a period of time.
+// This data is used to populate the Dashboard chart
+app.get('/chartData/:ticker/:timeframe/:startDate/:endDate', function(req, res) {
+    console.log("chart data endpoint");
+
+    ret = addon.chartData(req.params.ticker,req.params.timeframe,req.params.startDate,req.params.endDate);
+
+    res.send(ret);
+});
+
+// Run the shortest path algorithm and return the most profitable path between two currencies
+// and return the most profitable exchange rate using said path
+app.get('/arbitrageData/:startCurr/:endCurr/:maxNumberExchanges', function(req, res) {
+    console.log("arbitrage data endpoint");
+
+    if (req.query.exclude === undefined){
+        req.query.exclude = [];
+    }
+
+    ret = addon.arbitrageData(req.params.startCurr, req.params.endCurr, req.params.maxNumberExchanges, req.query.exclude);
+
+    res.send(ret);
+});
+
+
+// Return the most recent exchange rate between two currencies
+app.get('/calculatorData/:startCurr/:endCurr', function(req, res) {
+    console.log("calculator data endpoint");
+
+    ret = addon.calculatorData(req.params.startCurr, req.params.endCurr);
+
+    if(isNaN(ret["rate"])){
+        console.log("rate is not a number, returning 1.")
+        ret["rate"] = 1;
+    }
+
+    res.send(ret);
+});
